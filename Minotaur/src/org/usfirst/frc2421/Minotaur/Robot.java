182c9a3b348111a7d7f041088dbe641159a6ad45
RTR2421 <team2421coders@gmail.com>
2016-02-20 13:31:33 -0500

    Proper tankDrive command (made by Eyob, Zack the lesser and I) is now integrated into the main project.

--- Minotaur/src/org/usfirst/frc2421/Minotaur/Robot.java
+++ Minotaur/src/org/usfirst/frc2421/Minotaur/Robot.java
@@ -91,7 +91,8 @@ public class Robot extends IterativeRobot {
         // continue until interrupted by another command, remove
         // this line or comment it out.
         if (autonomousCommand != null) autonomousCommand.cancel();
-        Drive drive = new Drive();
+        motors.initDefaultCommand();
+        
     }
 
     /**
